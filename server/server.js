c82d50ce34492d6bb04e558f2936285049b38a5f
Rohan Rawat <149322525+RohanRawat16@users.noreply.github.com>
2024-10-03 14:26:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path'); // Required to correctly resolve paths
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('../')); // Serve static files from the root
+// Serve static files from root, 'css' and 'js' directories
+app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, '../css')));
+app.use(express.static(path.join(__dirname, '../js')));
 
 io.on('connection', (socket) => {
     socket.on('update', (data) => {
@@ -14,6 +18,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// Use the port provided by Vercel or default to 3000
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(Server is running on port ${PORT});
 });
